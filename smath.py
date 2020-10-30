9099f5c1622fcbdae4867785a98d01df1e0295c1
ThatScode24 <65173747+ThatScode24@users.noreply.github.com>
2020-10-30 15:42:19 +0300

    Added  x√root Major Feature
    Added a  x√root where the  x variable represents the 4 and the root represents the 125 in the expression 4√125. Did this using the *root (optional argument in the spl function) that takes the numbers before and after the ! (using ! instead of √ because square root symbol (√) is not on the keyboard, so 4√125  =  4!125 and √125  =  !125). Mapped the root variable to the number after the ! and the multp to the number before the ! (in this case the 4 is mapped to multp and the 125 to root) and used the .split() method for the program to return a list (later used with indexes for the algorithm). Also took some time to order the code and modified the start & end constants to 100 000 instead of 5000, for the program to have a wider range of possible simplifications (this slowed the algorithm down by 0.1 seconds, not a big deal! )

--- smath.py
+++ smath.py
@@ -32,42 +32,57 @@ def quadr(a, b, c):
         
         return f"The delta is negative (Δ = {delta} so Δ < 0). Therefore, there are no mathematical solutions possible. S = ø"
 
-def spl(root):
+def spl(*root):
     foundSolution = False
     primes = []
     nums = []
-    start, end = 0, 5000
+    start, end = 0, 100000
+    sep = root[0].split('!')
+    if sep[0] == '':
+        root = sep[1]
+        multp = None
+    else:
+        root = sep[1]
+        multp = sep[0]
     for i in range(start, end + 1):
         if (i**(.5) == int(i**(.5))):
             nums.append(i)
-    for i in range(1500):
-        primes.append(i)
     else:
-        for num in nums:
-            if root == num:
-                foundSolution = True
-                return str(int(sqrt(num)))
-            else:
-                pass
+        for i in range(100000):
+            primes.append(i)
         else:
-            for prime in primes:
-                if prime != 0:
-                    if root % prime == 0:
-                        r = root / prime
-                        s = sqrt(r)
-                        items = str(s).split(".")
-                        if foundSolution == True:
-                            pass
-                        else:
-                            if len(str(float(items[1]))) > 3:
+            for num in nums:
+                if root == num:
+                    foundSolution = True
+                    return str(int(sqrt(num)))
+                else:
+                    pass
+            else:
+                for prime in primes:
+                    if prime != 0:
+                        if int(root) % int(prime) == 0:
+                            r = int(root) / prime
+                            s = sqrt(r)
+                            items = str(s).split(".")
+                            if foundSolution == True:
                                 pass
                             else:
-                                if str(float(items[1])) == '0.0':
-                                    foundSolution = True
-                                    mod_s = int(s)
-                                    if mod_s == 1:
-                                        return str(f"√{prime}")
+                                if len(str(float(items[1]))) > 3:
+                                    pass
+                                else:
+                                    if multp == None:
+                                        if str(float(items[1])) == '0.0':
+                                            foundSolution = True
+                                            mod_s = int(s)
+                                            if mod_s == 1:
+                                                return str(f"√{prime}")
+                                            else:
+                                                return str(f"{mod_s}√{prime}")
                                     else:
-                                        return str(f"{mod_s}√{prime}")
-                else:
-                    foundSolution = False
+                                        if str(float(items[1])) == '0.0':
+                                            foundSolution = True
+                                            mod_s = int(s) * int(multp)
+                                            return str(f"{mod_s}√{prime}")
+
+                    else:
+                        foundSolution = False
